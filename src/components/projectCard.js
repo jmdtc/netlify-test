7b5431a042b012cb5a576c00ec5d795844f0e832
Mathieu Delisle <mathieu.delisle@tracksfortrucks.com>
2021-08-17 16:30:26 +0200

    clearly not the right branch

---
+++ src/components/projectCard.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import styled from "styled-components"
+
+
+const Card = styled.div`
+`
+
+const ProjectCard = ({ project }) => {
+    const { title, subtitle, image, path } = project
+    return (
+        <Card>
+
+        </Card>
+    )
+}
+
+export default ProjectCard
